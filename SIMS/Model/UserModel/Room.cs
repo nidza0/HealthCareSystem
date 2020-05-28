c8cb8441a6e73201cd990df4391b5a3c9d04b413
vucaj <vucaj1998@outlook.com>
2020-05-28 15:35:40 +0200

    Model-> ManagerModel konstruktori
    dodati konstruktori sa ID

--- SIMS/Model/UserModel/Room.cs
+++ SIMS/Model/UserModel/Room.cs
@@ -5,6 +5,7 @@
  ***********************************************************************/
 
 using System;
+using System.Collections.Generic;
 using SIMS.Model.ManagerModel;
 using SIMS.Repository.Abstract;
 
@@ -17,10 +18,20 @@ namespace SIMS.Model.UserModel
         private bool _occupied;
         private int _floor;
         private RoomType _roomType;
-        private InventoryItem _inventoryItem;
+        private List<InventoryItem> _inventoryItem;
 
-        public Room(string roomNumber, bool occupied, int floor, RoomType roomType, InventoryItem inventoryItem)
+        public Room(string roomNumber, bool occupied, int floor, RoomType roomType, List<InventoryItem> inventoryItem)
         {
+            _roomNumber = roomNumber;
+            _occupied = occupied;
+            _floor = floor;
+            _roomType = roomType;
+            _inventoryItem = inventoryItem;
+        }
+
+        public Room(long id, string roomNumber, bool occupied, int floor, RoomType roomType, List<InventoryItem> inventoryItem)
+        {
+            _id = id;
             _roomNumber = roomNumber;
             _occupied = occupied;
             _floor = floor;
@@ -32,7 +43,7 @@ namespace SIMS.Model.UserModel
         public bool Occupied { get => _occupied; set => _occupied = value; }
         public int Floor { get => _floor; set => _floor = value; }
         public RoomType RoomType { get => _roomType; set => _roomType = value; }
-        public InventoryItem InventoryItem { get => _inventoryItem; set => _inventoryItem = value; }
+        public List<InventoryItem> InventoryItem { get => _inventoryItem; set => _inventoryItem = value; }
 
 
 
