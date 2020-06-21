3ccfec35998cc4151513b93daa31e54d8386fb44
LukaKrickovic <krickovicluka@gmail.com>
2020-06-21 16:59:01 +0200

    AppointmentStrategy - Implemented

--- SIMS/Service/MedicalService/IAppointmentStrategy.cs
+++ SIMS/Service/MedicalService/IAppointmentStrategy.cs
@@ -10,11 +10,11 @@ namespace SIMS.Service.MedicalService
 {
     public interface IAppointmentStrategy
     {
-        bool CheckType(Appointment appointment);
+        void CheckType(Appointment appointment);
 
         void checkDateTimeValid(Appointment appointment);
 
-        int Validate(Appointment appointment);
+        void Validate(Appointment appointment);
 
         bool isAppointmentChangeable(Appointment appointment);
 
